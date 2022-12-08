f36cd3b7c935a4bcb33dee9b2073a3aecd92a04b
kevinqtogitty <103936802+kevinqtogitty@users.noreply.github.com>
2022-12-08 22:53:12 +0100

    added dialog to give user instructions on page load

---
+++ src/components/TipDialog.tsx
@@ -0,0 +1,48 @@
+import React, { useEffect, useState } from 'react';
+import { a, useSpring } from 'react-spring';
+
+const TipDialog = () => {
+  const [active, setActive] = useState(false);
+
+  useEffect(() => {
+    setActive(true);
+  }, []);
+
+  const overlaySpring = useSpring({
+    opacity: active ? 1 : 0
+  });
+
+  const dialogSpring = useSpring({
+    opacity: active ? 1 : 0,
+    transform: active ? 'translateY(0%)' : 'translateY(100%)'
+  });
+
+  const handleCloseDialog = () => {
+    setActive(false);
+  };
+
+  console.log(window.innerHeight, window.innerWidth);
+  return (
+    <>
+      <a.div
+        style={overlaySpring}
+        className="overlay"
+        onClick={handleCloseDialog}
+      >
+        <a.div style={dialogSpring} className="dialog">
+          <div className="tip-container">
+            <span className="tip">
+              Press the <span>'Enter'</span> key to get a random cocktail
+            </span>
+            <span>Happy drinkin&apos;! 🍸</span>
+            <span className="close-button" onClick={handleCloseDialog}>
+              X
+            </span>
+          </div>
+        </a.div>
+      </a.div>
+    </>
+  );
+};
+
+export default TipDialog;
