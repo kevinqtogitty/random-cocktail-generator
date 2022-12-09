c7fddc93cffa2e93147da6fb3c0c96f3f92b709f
kevinqtogitty <103936802+kevinqtogitty@users.noreply.github.com>
2022-12-09 23:34:44 +0100

    refactored keyboard event handler to trigger cocktail history menu

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import axios from 'axios';
 import { useEffect, useState } from 'react';
+import CocktailHistoryMenu from './components/CocktailHistoryMenu';
 import CocktailName from './components/CocktailName';
 import Instructions from './components/Instructions';
 import RecipeAndInstructions from './components/Recipe';
@@ -8,37 +9,53 @@ import './index.css';
 import { DrinkData, getRandomCocktail } from './requests/requests';
 
 function App() {
-  const [randomCocktail, setRandomCocktail] = useState<DrinkData>();
+  const [activeCocktail, setActiveCocktail] = useState<DrinkData>();
+  const [cocktailHistory, setCocktailHistory] = useState<DrinkData[]>([]);
+  const [triggerCocktailHistoryByClick, setTriggerCocktailHistoryByClick] =
+    useState(false);
+  const [cocktailHistoryMenuActive, setCocktailHistoryMenuActive] =
+    useState(false);
 
   useEffect(() => {
-    document.addEventListener('keydown', fetchNewDrinkOnKeyPress);
-    return () =>
-      document.removeEventListener('keydown', fetchNewDrinkOnKeyPress);
+    document.addEventListener('keydown', keyboardEvent);
+    return () => document.removeEventListener('keydown', keyboardEvent);
   });
 
   useEffect(() => {
     fetchData();
+    console.log('here');
   }, []);
 
-  const fetchData = async () => {
+  const fetchData = async (): Promise<void> => {
     try {
       const data = await getRandomCocktail();
-      setRandomCocktail(data);
+      setActiveCocktail(data);
+      setCocktailHistory((state) => [...state, data!]);
     } catch (e) {
       console.log(e);
     }
   };
 
-  const fetchNewDrinkOnKeyPress = (e: KeyboardEvent) => {
-    if (e.key === 'Enter') return fetchData();
+  const keyboardEvent = (e: KeyboardEvent): Promise<void> | void => {
+    if (e.key === 'Enter') {
+      return fetchData();
+    } else if (e.key === 'h' || triggerCocktailHistoryByClick === true) {
+      return setCocktailHistoryMenuActive((state) => !state);
+    }
   };
 
   return (
     <div className="mainContainer">
-      <CocktailName randomCocktail={randomCocktail!} />
-      <RecipeAndInstructions drinkData={randomCocktail?.ingredients} />
-      <Instructions instructions={randomCocktail?.instructions} />
+      <CocktailName activeCocktail={activeCocktail!} />
+      <RecipeAndInstructions drinkData={activeCocktail?.ingredients} />
+      <Instructions instructions={activeCocktail?.instructions} />
       <TipDialog />
+      <CocktailHistoryMenu
+        setActiveCocktail={setActiveCocktail}
+        cocktailHistory={cocktailHistory}
+        cocktailHistoryMenuActive={cocktailHistoryMenuActive}
+        setCocktailHistoryMenuActive={setCocktailHistoryMenuActive}
+      />
     </div>
   );
 }
